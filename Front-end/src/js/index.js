8b897808e6ea8b31ef002b27e0bd39085031e08b
popibeatrice <popicabeatrice31@gmail.com>
2023-06-02 19:47:26 +0300

    nu i gata cu traseele

--- Front-end/src/js/index.js
+++ Front-end/src/js/index.js
@@ -147,8 +147,8 @@ const introductiveText = document.querySelectorAll(".text-introductiv");
 window.addEventListener("scroll", () => {
     introductiveText.forEach((text) => {
         if (
-            text.getBoundingClientRect().top > window.innerHeight / 2 + 100 ||
-            text.getBoundingClientRect().top < window.innerHeight / 3 - 100
+            text.getBoundingClientRect().top > window.innerHeight / 2 + 180 ||
+            text.getBoundingClientRect().top < window.innerHeight / 3 - 180
         ) {
             text.classList.add("opacity-10", "scale-100");
             text.classList.remove("opacity-100", "scale-105");
@@ -198,7 +198,7 @@ ScrollReveal().reveal("#weather", {
 
 const temp = document.querySelector(".temp");
 const oras = document.querySelectorAll(".ora");
-const iconVreme = document.querySelector("#icon-vreme");
+const iconVreme = document.querySelector("#vreme-icon");
 const pmAM = document.querySelectorAll(".pm-am");
 
 function updateLocalTime() {
@@ -235,9 +235,9 @@ window.onload = async () => {
             vremeInfo.data.text === "Overcast"
         )
             iconVreme.src = cloudy;
-        else if (vremeInfo.data.text === "Partly cloudy")
+        else if (vremeInfo.data.text === "Partly cloudy") {
             iconVreme.src = semiCloudy;
-        else if (
+        } else if (
             vremeInfo.data.text === "Thundery outbreaks possible" ||
             vremeInfo.data.text === "Moderate rain at times" ||
             vremeInfo.data.text === "Moderate rain" ||
@@ -269,4 +269,99 @@ window.onload = async () => {
     }
 };
 
-setInterval(updateLocalTime, 1000);
+console.log("pipi");
+
+(function () {
+    var slidersContainer = document.querySelector(".sliders-container");
+
+    // Initializing the numbers slider
+    var msNumbers = new MomentumSlider({
+        el: slidersContainer,
+        cssClass: "ms--numbers",
+        range: [1, 4],
+        rangeContent: function (i) {
+            return "0" + i;
+        },
+        style: {
+            transform: [{ scale: [0.4, 1] }],
+            opacity: [0, 1],
+        },
+        interactive: false,
+    });
+
+    // Initializing the titles slider
+    var titles = [
+        "King of the Ring Fight",
+        "Sound of Streets",
+        "Urban Fashion",
+        "Windy Sunset",
+    ];
+    var msTitles = new MomentumSlider({
+        el: slidersContainer,
+        cssClass: "ms--titles",
+        range: [0, 3],
+        rangeContent: function (i) {
+            return "<h3>" + titles[i] + "</h3>";
+        },
+        vertical: true,
+        reverse: true,
+        style: {
+            opacity: [0, 1],
+        },
+        interactive: false,
+    });
+
+    // Initializing the links slider
+    var msLinks = new MomentumSlider({
+        el: slidersContainer,
+        cssClass: "ms--links",
+        range: [0, 3],
+        rangeContent: function () {
+            return '<a class="ms-slide__link">View Case</a>';
+        },
+        vertical: true,
+        interactive: false,
+    });
+
+    // Get pagination items
+    var pagination = document.querySelector(".pagination");
+    var paginationItems = [].slice.call(pagination.children);
+
+    // Initializing the images slider
+    var msImages = new MomentumSlider({
+        // Element to append the slider
+        el: slidersContainer,
+        // CSS class to reference the slider
+        cssClass: "ms--images",
+        // Generate the 4 slides required
+        range: [0, 3],
+        rangeContent: function () {
+            return '<div class="ms-slide__image-container"><div class="ms-slide__image"></div></div>';
+        },
+        // Syncronize the other sliders
+        sync: [msNumbers, msTitles, msLinks],
+        // Styles to interpolate as we move the slider
+        style: {
+            ".ms-slide__image": {
+                transform: [{ scale: [1.5, 1] }],
+            },
+        },
+        // Update pagination if slider change
+        change: function (newIndex, oldIndex) {
+            if (typeof oldIndex !== "undefined") {
+                paginationItems[oldIndex].classList.remove(
+                    "pagination__item--active"
+                );
+            }
+            paginationItems[newIndex].classList.add("pagination__item--active");
+        },
+    });
+
+    // Select corresponding slider item when a pagination button is clicked
+    pagination.addEventListener("click", function (e) {
+        if (e.target.matches(".pagination__button")) {
+            var index = paginationItems.indexOf(e.target.parentNode);
+            msImages.select(index);
+        }
+    });
+})();
