90385042aa27f2ac094832b1938d2c1067fd26fc
popibeatrice <popicabeatrice31@gmail.com>
2023-06-04 22:41:31 +0300

    gata de trasee

--- Front-end/webpack.config.js
+++ Front-end/webpack.config.js
@@ -1,67 +1,78 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
-  entry: {
-    index: './src/js/index.js',
-    indexEn: './src/js/indexEn.js',
-  },
-  mode: 'development',
-  devServer: {
-    watchFiles: ['src/**/*'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+    entry: {
+        index: "./src/js/index.js",
+        indexEn: "./src/js/indexEn.js",
+        trasee_pe_jos: "./src/js/trasee.js",
+    },
+    mode: "development",
+    devServer: {
+        watchFiles: ["src/**/*"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
 
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
 
-      {
-        test: /\.(jpg|png|jpeg|svg|gif|pdf)$/,
-        type: 'asset/resource',
-      },
+            {
+                test: /\.(jpg|png|jpeg|svg|gif|pdf)$/,
+                type: "asset/resource",
+            },
 
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, "src"),
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [`...`, new CssMinimizerPlugin()],
+    },
+    resolve: {
+        extensions: ["*", ".js"],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            // Also generate a test.html
+            filename: "index.html",
+            template: "src/html/index.html",
+            chunks: ["index"],
+        }),
+        new HtmlWebpackPlugin({
+            // Also generate a test.html
+            filename: "indexEn.html",
+            template: "src/html/indexEn.html",
+            chunks: ["indexEn"],
+        }),
+        new HtmlWebpackPlugin({
+            // Also generate a test.html
+            filename: "trasee.html",
+            template: "src/html/trasee.html",
+            chunks: ["trasee"],
+        }),
     ],
-  },
-  optimization: {
-    minimizer: [`...`, new CssMinimizerPlugin()],
-  },
-  resolve: {
-    extensions: ['*', '.js'],
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      // Also generate a test.html
-      filename: 'index.html',
-      template: 'src/html/index.html',
-      chunks: ['index'],
-    }),
-    new HtmlWebpackPlugin({
-      // Also generate a test.html
-      filename: 'indexEn.html',
-      template: 'src/html/indexEn.html',
-      chunks: ['indexEn'],
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    clean: true,
-  },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "public"),
+        clean: true,
+    },
 };
