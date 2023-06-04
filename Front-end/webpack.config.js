734caeeae0702751016d28d6ea33bfabf5b58f42
popibeatrice <popicabeatrice31@gmail.com>
2023-06-04 22:59:47 +0300

    gata de trasee

--- Front-end/webpack.config.js
+++ Front-end/webpack.config.js
@@ -3,11 +3,11 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
-<<<<<<< HEAD
     entry: {
         index: "./src/js/index.js",
         indexEn: "./src/js/indexEn.js",
-        trasee_pe_jos: "./src/js/trasee.js",
+        trasee_pe_jos: "./src/js/trasee_pe_jos.js",
+        obiective: "./src/js/obiective.js",
     },
     mode: "development",
     devServer: {
@@ -22,26 +22,6 @@ module.exports = {
                     loader: "babel-loader",
                 },
             },
-=======
-  entry: {
-    index: './src/js/index.js',
-    indexEn: './src/js/indexEn.js',
-    obiective: './src/js/obiective.js',
-  },
-  mode: 'development',
-  devServer: {
-    watchFiles: ['src/**/*'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
->>>>>>> 77f893353065d2a8015f5b2d140875c6307d05c5
 
             {
                 test: /\.html$/i,
@@ -86,50 +66,20 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             // Also generate a test.html
-            filename: "trasee.html",
-            template: "src/html/trasee.html",
-            chunks: ["trasee"],
+            filename: "trasee_pe_jos.html",
+            template: "src/html/trasee_pe_jos.html",
+            chunks: ["trasee_pe_jos"],
+        }),
+        new HtmlWebpackPlugin({
+            // Also generate a test.html
+            filename: "obiective.html",
+            template: "src/html/obiective.html",
+            chunks: ["obiective"],
         }),
     ],
-<<<<<<< HEAD
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "public"),
         clean: true,
     },
-=======
-  },
-  optimization: {
-    minimizer: [`...`, new CssMinimizerPlugin()],
-  },
-  resolve: {
-    extensions: ['*', '.js'],
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      // Also generate a test.html
-      filename: 'index.html',
-      template: 'src/html/index.html',
-      chunks: ['index'],
-    }),
-    new HtmlWebpackPlugin({
-      // Also generate a test.html
-      filename: 'indexEn.html',
-      template: 'src/html/indexEn.html',
-      chunks: ['indexEn'],
-    }),
-    new HtmlWebpackPlugin({
-      // Also generate a test.html
-      filename: 'obiective.html',
-      template: 'src/html/obiective.html',
-      chunks: ['obiective'],
-    }),
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    clean: true,
-  },
->>>>>>> 77f893353065d2a8015f5b2d140875c6307d05c5
 };
