2f152b570e59c3a6ab7510bfc5d09561cc17e2bd
DavidPav3l <david_marian@outlook.com>
2023-06-13 23:34:07 +0300

    avem rute la pagini

--- Back-end/server.js
+++ Back-end/server.js
@@ -24,10 +24,81 @@ app.get('/en', async (req, res) => {
     .status(200)
     .sendFile(path.join(__dirname, '../Front-end/public/indexEn.html'));
 });
+
+app.get('/admin/login', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/login.html'));
+});
+
 app.get('/admin', async (req, res) => {
   res
     .status(200)
     .sendFile(path.join(__dirname, '../Front-end/public/indexAdmin.html'));
+});
+
+app.get('/obiective', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/obiective.html'));
+});
+
+app.get('/sights', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/obiective.html'));
+});
+
+app.get('/routes/car', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/trasee_masina.html'));
+});
+
+app.get('/routes/onfoot', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/trasee_pe_jos.html'));
+});
+
+app.get('/routes/bike', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(
+      path.join(__dirname, '../Front-end/public/trasee_bicicleta.html')
+    );
+});
+
+app.get('/trasee/masina', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/trasee_masina.html'));
+});
+
+app.get('/trasee/pejos', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/trasee_pe_jos.html'));
+});
+
+app.get('/trasee/bicicleta', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(
+      path.join(__dirname, '../Front-end/public/trasee_bicicleta.html')
+    );
+});
+
+app.get('/evenimente', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/obiective.html'));
+});
+
+app.get('/events', async (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, '../Front-end/public/obiective.html'));
 });
 
 const start = async () => {
