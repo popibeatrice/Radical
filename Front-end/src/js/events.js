4731deed6c63498807057245f879cf2a36883c98
popibeatrice <popicabeatrice31@gmail.com>
2023-06-19 14:45:02 +0300

    ajustari ajustari

--- Front-end/src/js/events.js
+++ Front-end/src/js/events.js
@@ -1,67 +1,67 @@
-import './vreme';
-import './header';
-import '../styles/header.css';
-import '../styles/evenimente.css';
-import '../styles/tailwind.css';
-import axios from 'axios';
+import "./vreme";
+import "./header";
+import "../styles/header.css";
+import "../styles/evenimente.css";
+import "../styles/tailwind.css";
+import axios from "axios";
 
 // RO SWITCH
-const roSwitchs = document.querySelectorAll('.ro-switch');
+const roSwitchs = document.querySelectorAll(".ro-switch");
 
 roSwitchs.forEach((roSwitch) => {
-  roSwitch.addEventListener('click', () => {
-    location.href = location.origin;
-  });
+    roSwitch.addEventListener("click", () => {
+        location.href = location.origin;
+    });
 });
 
-const gridCont = document.querySelector('.cont');
+const gridCont = document.querySelector(".cont");
 
 const RenderEvent = (id) => {
-  const img = document.createElement('img');
+    const img = document.createElement("img");
 
-  img.classList.add('object-contain', 'w-full');
-  img.src = `https://visitvaslui.fra1.digitaloceanspaces.com/${id}/poster.jpg`;
+    img.classList.add("object-contain", "w-full");
+    img.src = `https://visitvaslui.fra1.digitaloceanspaces.com/${id}/poster.jpg`;
 
-  gridCont.appendChild(img);
+    gridCont.appendChild(img);
 };
 
 const GetEvents = async () => {
-  try {
-    const res = await axios.get('/evenimente/events');
-    const sights = res.data.events;
-    if (sights.length !== 0) {
-      gridCont.innerHTML = '';
-      gridCont.classList.add('grid-cont');
-      sights.forEach((event) => {
-        RenderEvent(event._id);
-      });
+    try {
+        const res = await axios.get("/evenimente/events");
+        const sights = res.data.events;
+        if (sights.length !== 0) {
+            gridCont.innerHTML = "";
+            gridCont.classList.add("grid-cont");
+            sights.forEach((event) => {
+                RenderEvent(event._id);
+            });
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 GetEvents();
 
-var swiperHero = new Swiper('.swiper-hero', {
-  effect: 'fade',
-  autoplay: {
-    delay: 3000,
-  },
-  speed: 1000,
+var swiperHero = new Swiper(".swiper-hero", {
+    effect: "fade",
+    autoplay: {
+        delay: 1000,
+    },
+    speed: 1000,
 });
 
-const swiper = new Swiper('.swiper', {
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-  },
+const swiper = new Swiper(".swiper", {
+    effect: "fade",
+    fadeEffect: {
+        crossFade: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+    },
 });
